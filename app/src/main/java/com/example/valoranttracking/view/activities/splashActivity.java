0c2ec7175600245d24581c6d9a14685a5e51b9af
Felix <83955089+Flxjnthn@users.noreply.github.com>
2022-12-29 12:23:44 +0700

    Add splash screen, fixed botnav

--- app/src/main/java/com/example/valoranttracking/view/activities/splashActivity.java
+++ app/src/main/java/com/example/valoranttracking/view/activities/splashActivity.java
@@ -2,7 +2,9 @@ package com.example.valoranttracking.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.valoranttracking.R;
 
@@ -12,6 +14,13 @@ public class splashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            //habis splash screen lalu pindah ke login
+            Intent agent = new Intent(this, mainMenuActivity.class);
+            startActivity(agent);
+            finish();
+        },2000);
 
 
     }
