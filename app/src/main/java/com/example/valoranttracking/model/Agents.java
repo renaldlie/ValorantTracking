cabb9fd4a72c6f3d82b7b09e27db484f6eff273e
ardi12345678978 <benedictusardi1@gmail.com>
2023-01-04 21:38:02 +0700

    ubah agent

--- app/src/main/java/com/example/valoranttracking/model/Agents.java
+++ app/src/main/java/com/example/valoranttracking/model/Agents.java
@@ -11,33 +11,6 @@ import java.util.ArrayList;
 
 public class Agents {
 
-    @SerializedName("agents_name")
-    private String agents_name;
-    @SerializedName("agents_role")
-    private String agents_role;
-
-
-
-    protected Agents(Parcel in) {
-        this.agents_name = agents_name;
-        this.agents_role = agents_role;
-    }
-
-    public String getAgents_name() {
-        return agents_name;
-    }
-
-    public void setAgents_name(String agents_name) {
-        agents_name = agents_name;
-    }
-
-    public String getAgents_role() {
-        return agents_role;
-    }
-
-    public void setAgents_role(String agents_role) {
-        agents_role = agents_role;
-    }
-
+    
 }
 
