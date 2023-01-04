400926778b5eb7962c9730464e6b8c6377878389
ardi12345678978 <benedictusardi1@gmail.com>
2023-01-04 21:39:51 +0700

    Update Maps.java

--- app/src/main/java/com/example/valoranttracking/model/Maps.java
+++ app/src/main/java/com/example/valoranttracking/model/Maps.java
@@ -4,5 +4,30 @@ import com.google.gson.annotations.SerializedName;
 
 public class Maps {
 
-    
+    @SerializedName("map_name")
+    private String map_name;
+    @SerializedName("map_icon")
+    private String map_icon;
+
+    public Maps(String map_name, String map_icon) {
+        this.map_name = map_name;
+        this.map_icon = map_icon;
+    }
+
+    public String getMap_name() {
+        return map_name;
+    }
+
+    public void setMap_name(String map_name) {
+        this.map_name = map_name;
+    }
+
+    public String getMap_icon() {
+        return map_icon;
+    }
+
+    public void setMap_icon(String map_icon) {
+        this.map_icon = map_icon;
+    }
+
 }
